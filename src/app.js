9a460fcce1db3cfcb9e6fcb2417b321b0268dd66
LunaB28 <lunab2815@gmail.com>
2025-04-11 23:26:43 +0000

    Generador funcional, color adaptado a cada pinta index.html: añadí un boton con el que ahora se activa el funcionamiento también cambié la posición a estar justificado a la izquierda. style.css: añadí un color al boton, asi como a su color con el mouse encima y al ser activo, también ajustpe la posición del botón index.html: generé el if else para el cambio de color de rojo o negro, así como la impresión del simbolo central

--- src/app.js
+++ src/app.js
@@ -6,26 +6,25 @@ function Random(array) {
   return Math.floor(Math.random() * array.length);
 }
 window.onload = function() {
-  let Symbols = [
-    "A",
-    "2",
-    "3",
-    "4",
-    "5",
-    "6",
-    "7",
-    "8",
-    "9",
-    "10",
-    "J",
-    "Q",
-    "K"
-  ];
-  let cornerPosibleIndex = ["♣", "♠", "♦", "♥"];
-  let Symbol = Random(Symbols);
-  let cornerIndex = Random(cornerPosibleIndex);
-  let top = document.getElementById("topCornerIndex");
-  let bottom = document.getElementById("bottomCornerIndex");
-  top.innerText = cornerPosibleIndex[cornerIndex];
-  bottom.innerText = cornerPosibleIndex[cornerIndex];
+  let randomButton = document.getElementById("randomButton");
+  randomButton.addEventListener("click", function() {
+    let symbols = [ "A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
+    let cornerPosibleIndex = ["♣", "♠", "♦", "♥"];
+    let symbol = Random(symbols);
+    let cornerIndex = Random(cornerPosibleIndex);
+    let top = document.getElementById("topCornerIndex");
+    let bottom = document.getElementById("bottomCornerIndex");
+    let center = document.getElementById("centerRank");
+    top.innerText = cornerPosibleIndex[cornerIndex];
+    bottom.innerText = cornerPosibleIndex[cornerIndex];
+    center.innerText = symbols[symbol];
+    if (cornerIndex >= 2) {
+      top.style.color = "red";
+      bottom.style.color = "red";
+      return;
+    } else {
+      top.style.color = "black";
+      bottom.style.color = "black";
+    }
+  });
 };
